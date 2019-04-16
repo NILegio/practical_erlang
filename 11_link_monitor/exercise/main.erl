d83b4ae8dcf5b5705c214f679998dc02b56b6480
kalinin <kalinin@debian.home>
2019-04-16 08:28:28 +0300

    homework11_v095

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -1,7 +1,35 @@
 -module(main).
 
--export([parse/1]).
+-export([parse/1, loop/2, aggregate/2]).
 
 
 parse(Files) ->
-    ok.
+    io:format("reducer ~p ~n", [self()]),
+    Wait =
+        lists:map(fun (File)->
+        WRef = make_ref(),
+        WPid = spawn(worker, worker1, [self(), WRef, File]),
+            MRef = erlang:monitor(process, WPid), {MRef, WRef, WPid, File} end, Files),
+    io:format("Wait:~p~n", [Wait]),
+    loop(Wait, {#{}, #{}}).
+
+loop([],  Temp)-> Temp;
+loop([{MRef, Ref, _, File}|Wait], {Acc, ErrorAcc})->
+    receive
+        {data, Ref, Res} -> loop(Wait, {aggregate(Res, Acc), ErrorAcc});
+        {'DOWN', MRef, process, Pid, {TrueReason, _}} ->
+            io:format("Worker ~p dont work~n", [Pid]),
+            loop(Wait, {Acc, ErrorAcc#{File=>TrueReason}})
+    after
+        1000 -> {error, not_reply}
+    end.
+
+
+aggregate(Map1, Map2) ->
+    maps:fold(
+        fun(K,V, Acc)->
+            case maps:find(K, Acc)  of
+            {ok, Va}-> Acc#{K := V + Va};
+            error -> Acc#{K => V}
+        end end, Map1, Map2).
+
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,21 @@
 -module(worker).
+-export([worker1/3, parse_file/1]).
+%%-export([worker1/2, parse_file/1]).
+
+%%worker1(ReducerPid, File)->
+worker1(ReducerPid, Ref, File)->
+  io:format("worker ~p~n", [self()]),
+  Res = parse_file(File),
+  ReducerPid ! {data, Ref, Res}, ok.
+%%  ReducerPid ! {data, Res}, ok.
+
+parse_file(File)->
+  {ok, Content} = file:read_file(File),
+  Str = binary:split(Content, [<<"\n">>], [global]),
+  lists:foldl(fun (Word, Acc) -> Word1 = binary:split(Word, <<",">>, [global]),
+    case list_to_tuple(Word1) of
+      {_, Veg, AmountStr, _} -> {Amount, _} = string:to_integer(binary:bin_to_list(AmountStr)),
+      Acc#{Veg => Amount};
+
+      {<<>>} -> Acc end
+    end, #{}, Str).
