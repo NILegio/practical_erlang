aad12504192b066c8e77c6da9604dbedccbdc3f9
kalinin <kalinin@debian.home>
2019-04-22 11:15:16 +0300

    homework14_v095

--- 14_otp_project/exercise/my_crypt/apps/my_crypt/src/my_crypt.erl
+++ 14_otp_project/exercise/my_crypt/apps/my_crypt/src/my_crypt.erl
@@ -12,7 +12,7 @@
 %% API
 -behavior(gen_server).
 
--export([start_link/0, get_key/0, set_key/1, encode/1]).
+-export([start_link/0, get_key/0, set_key/1, encode/1, hash/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 
@@ -38,12 +38,18 @@ encode(Str)
   ->
   gen_server:call(?MODULE, {encode, Str}).
 
+hash(Str) ->
+  gen_server:call(?MODULE, {hash, Str}).
+
 
 %%% gen_server API
 
 init([]) ->
+  {ok, {A,B,C}} = application:get_env(my_crypt, seed),
+  rand:seed(exsplus, {A,B,C}),
+  Hash_table = gen_hash_table(),
   {ok, Key} = application:get_env(my_crypt, crypt_key),
-  State = #{key => Key},
+  State = #{key => Key, hash_table => Hash_table},
   {ok, State}.
 
 handle_call({get_key}, _Form, State)->
@@ -53,6 +59,10 @@ handle_call({get_key}, _Form, State)->
 handle_call({encode, Str}, _Form, State)->
   Encode_Str = in_encode(Str, maps:get(key,State)),
   {reply, Encode_Str, State};
+
+handle_call({hash, Str}, _Form, State)->
+  Hash = in_hash(Str, maps:get(hash_table,State)),
+  {reply, Hash, State};
 
 handle_call(_Request, _From, State) ->
   {reply, ok, State}.
@@ -83,6 +93,35 @@ in_encode(Str, [], Temp, OKey) -> in_encode(Str, OKey, Temp, OKey);
 in_encode([Char|Str], [Key_Char|Key], Temp, OKey)->
   NewChar = Char bxor Key_Char,
   in_encode(Str, Key, [NewChar|Temp], OKey).
+
+in_hash(Str, HashTable)->
+  Message = binary_to_list(Str),
+  {ok, Hash_Size} = application:get_env(my_crypt, hash_size),
+  BinaryHash = lists:map(fun (H) -> Hash = pearson_hash(Message, HashTable, H),
+    formatHash(Hash) end, lists:seq(1, Hash_Size div 2)),
+  unicode:characters_to_binary(BinaryHash).
+
+pearson_hash([], _HashTable, Hash)-> Hash;
+pearson_hash([Char|Message], HashTable, Hash)->
+  Position = Char bxor Hash,
+  Hash1 = lists:nth(Position+1, HashTable),
+  pearson_hash(Message, HashTable, Hash1).
+
+gen_hash_table()->
+  Example_table = lists:seq(0, 255),
+  shuffle(Example_table).
+
+shuffle(List) ->
+  Random_list = [{rand:uniform(), X}|| X <- List],
+  [X || {_, X}<- lists:sort(Random_list)].
+
+formatHash(Int)->
+  if
+    Int < 16 -> <<"0", (integer_to_binary(Int, 16))/binary>>;
+    true -> integer_to_binary(Int, 16)
+  end.
+
+
 
 
 
